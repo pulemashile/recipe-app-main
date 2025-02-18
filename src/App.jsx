2716e7feeb4380441e9bcfc6e0a0b6a50b45f005
pulemashile <166323606+pulemashile@users.noreply.github.com>
2025-02-18 14:46:58 +0200

    button

--- src/App.jsx
+++ src/App.jsx
@@ -142,13 +142,13 @@ const RecipeForm = ({ onSubmit }) => {
 
                 <div className="mt-10 sm:flex sm:items-center sm:space-x-8">
                   <a href="#" title="" className="inline-flex items-center justify-center px-10 py-4 text-base font-semibold text-white transition-all duration-200 bg-[#D80032] hover:bg-[#D80032] focus:bg-orange-600" role="button"> Start exploring </a>
-                  <a href="#" title="" className="inline-flex items-center mt-6 text-base font-semibold transition-all duration-200 sm:mt-0 hover:opacity-80">
+                  {/* <a href="#" title="" className="inline-flex items-center mt-6 text-base font-semibold transition-all duration-200 sm:mt-0 hover:opacity-80">
                     <svg className="w-10 h-10 mr-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                       <path fill="#D80032" stroke="#F97316" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z" />
                       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
                     Watch video
-                  </a>
+                  </a> */}
                 </div>
               </div>
 
